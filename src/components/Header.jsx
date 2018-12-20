8cc09fdac8d184fdf13d400021afa016492882d3
its-gio <me@its-g.io>
2018-12-19 20:59:21 -0800

    Adding slight styles to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <img
-        className="header__logo"
-        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
-        alt="Problem?"
-      />
-      <p className="header__tagline">Meme Generator</p>
+      <div className="header__image-container">
+        <img
+          className="header__image-container--image"
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+          alt="Problem?"
+        />
+      </div>
+      <h1 className="header__tagline">Meme Generator</h1>
     </header>
   );
 }
