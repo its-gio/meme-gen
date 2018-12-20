e730007b15922b15cc3c0c60c843bcf7e9155e9d
its-gio <me@its-g.io>
2018-12-20 11:15:41 -0800

    Changing Form to functional component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,27 @@
 import React, { Component } from "react";
 
-class Form extends Component {
-  render() {
-    return (
-      <form className="generator__form">
-        <input
-          className="generator__form--top-text"
-          name="topText"
-          onChange={this.props.textChange}
-          value={this.props.topText}
-          type="text"
-          placeholder="Top Text"
-        />
-        <input
-          className="generator__form--bottom-text"
-          name="bottomText"
-          onChange={this.props.textChange}
-          value={this.props.BottomText}
-          type="text"
-          placeholder="Bottom Text"
-        />
-        <button className="generator__form--button">Generate!</button>
-      </form>
-    );
-  }
+function Form() {
+  return (
+    <form className="generator__form">
+      <input
+        className="generator__form--top-text"
+        name="topText"
+        onChange={this.props.textChange}
+        value={this.props.topText}
+        type="text"
+        placeholder="Top Text"
+      />
+      <input
+        className="generator__form--bottom-text"
+        name="bottomText"
+        onChange={this.props.textChange}
+        value={this.props.BottomText}
+        type="text"
+        placeholder="Bottom Text"
+      />
+      <button className="generator__form--button">Generate!</button>
+    </form>
+  );
 }
 
 export default Form;
