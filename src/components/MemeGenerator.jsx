07e1ec5854451943b587e5881ddb363918541fb4
its-gio <me@its-g.io>
2018-12-19 21:16:35 -0800

    Changed background of header (Navy Blue) and Created state for MemeGenerator component

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class MemeGenerator extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      topText: "",
+      bottomText: "",
+      img: "http://i.imgflip.com/1bij.jpg"
+    };
+  }
+
   render() {
     return <div>I'm a meme generator</div>;
   }
