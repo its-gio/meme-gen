e0ff51a22f8c99c8ce0240efb45d79c0fa9997f4
its-gio <me@its-g.io>
2018-12-20 15:36:44 -0800

    Form inputs now have sweet animations (still needs tweaking ~~ button borders)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,27 +3,32 @@ import React from "react";
 function Form(props) {
   return (
     <form className="form">
-      <input
-        className="form__top-text"
-        name="topText"
-        onChange={props.textChange}
-        value={props.topText}
-        type="text"
-      />
-      <label className="form__top-text--label" htmlFor="topText">
-        Top Text
-      </label>
-      <input
-        className="form__bottom-text"
-        name="bottomText"
-        onChange={props.textChange}
-        value={props.BottomText}
-        type="text"
-        placeholder="Bottom Text"
-      />
-      <label className="form__bottom-text--label" htmlFor="bottomText">
-        Bottom Text
-      </label>
+      <div className="form__text-container">
+        <input
+          className="form__top-text"
+          name="topText"
+          onChange={props.textChange}
+          value={props.topText}
+          type="text"
+          required
+        />
+        <label className="form__top-text--label" htmlFor="topText">
+          Top Text
+        </label>
+      </div>
+      <div className="form__text-container">
+        <input
+          className="form__bottom-text"
+          name="bottomText"
+          onChange={props.textChange}
+          value={props.BottomText}
+          type="text"
+          required
+        />
+        <label className="form__bottom-text--label" htmlFor="bottomText">
+          Bottom Text
+        </label>
+      </div>
       <button className="form__button">Generate!</button>
     </form>
   );
